92b72dc79ca1b954430030d4caf3cf5e4c7792cf
TRICKSTAR\minht <rinnegan101220@gmail.com>
2021-12-21 09:02:38 +0700

    update noel

---
+++ eventChristmas/key.go
@@ -0,0 +1,36 @@
+key 
+- "countticket"
+- "countevent"
+- "eventdaily"
+- "checkfull"
+
+
+- "currentpos"
+- "countallevent"
+- "checkmistake"
+
+
+===========
+bug con ma vẫn ở ô quà đc quà 2 lần
+
+===========
+
+
+19 : khí vận : 
+- mất hết số xúc xắc hiện có ( 2/10 )
+- Thả thính 1 Mod khác giới vài câu ( 3/10 )
+- Nhảy đến ô 31 ( 2/10 )
+- Được tặng 2 viên xúc xắc ( 3/10 )
+30 : Quà : 20.000owo + Role Noel 
+50 : khí vận : 
+- Nhảy đến vô 57 ( 2/10 )
+- Quà vòng này đã bị cướp, Donate 20.000 để bắt trộm ( 3/10 )
+- Nhận 2 xúc xắc miễn phí ( 3/10 )
+- Nhận Roles Noel ( 2/10 )
+55 : Quà : 50.000owo 
+85 : Quà 80.000owo 
+100 : Quà
+- 500.000owo : ( 1/10 )
+- 200.000owo : ( 2/10 )
+- 100.000owo ( 7/10 )
+Hết vòng : 100.000owo
