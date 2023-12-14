f82b41589e04417dd7ca702d923acaba0b846684
trickstarcandina <rinnegan101220@gmail.com>
2023-12-14 22:25:18 +0700

    update logic gacha

---
+++ eventChristmas2023/key.go
@@ -0,0 +1,32 @@
+currentpos -> vị trí
+countticket -> đếm số xúc sắc
+countpineal -> đếm số thông
+
+{{/* 
+key 
+- "countevent"
+- "eventdaily"
+- "checkfull"
+
+    // 4, 26, 77, 97 : Móc mạnh lên, được ( 1-80% / 2 -20% ) quả thông rồi, thu thập thêm để đổi quà nhé ! 
+7, 32, 69, 86 : Hộp quà*
+    // 16 : Bão tuyết cuốn bạn đến ô 20 rồi, thích thật ! 
+    // 20 : Bão tuyết cuốn bạn đến ô 16 rồi, thích thật ! 
+    // 18, 56 : Nhặt được đôi vớ, phải làm nhiệm vụ để vượt qua nha, nhiệm vụ lần này : 
+    //     Lên ⁠🎀┊thời-sự gửi lời chào ( 25% ) / 
+    //     Lên ⁠💫┊share chia sẻ 1 tấm hình bạn thấy vui ( 50% ) / 
+    //     Treo Voice 15 phút tại Server ( 25% )
+    // 27 : Merry Christmas ! 
+28, 96 : Nhắc bạn : Vào #rung-chuông nhận quà nhé ! 
+    // 41, 72 : Chúc mừng bạn nhặt được 2 quả thông trên đường đi
+    // 45 : Chúc mừng bạn luồn qua ống khói đến được ô 61
+    // 48, 91 : Chúc mừng bạn nhận thêm 1 xí ngầu ❤️ 
+    // 51 : Chúc mừng bạn nhặt được 1 quả thông trên đường đi
+    // 58 : Hố đen đưa bạn đến ô 64 rồi, thích thật ! 
+    // 64 : Hố đen đưa bạn đến ô 58 rồi, thích thật ! 
+
+16 <-> 20
+45 -> 61
+58 <-> 64
+
+*/}}
