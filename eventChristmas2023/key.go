76ebbfbe95ef018064d06583db024dd311fc826c
trickstarcandina <rinnegan101220@gmail.com>
2023-12-17 10:23:41 +0700

    update: logic(claim,daily,gacha) event noel 2023

--- eventChristmas2023/key.go
+++ eventChristmas2023/key.go
@@ -2,6 +2,10 @@ currentpos -> vị trí
 countticket -> đếm số xúc sắc
 countpineal -> đếm số thông
 
+countevent -> đếm msg
+checkfull -> check chat
+eventdaily -> check daily
+
 {{/* 
 key 
 - "countevent"
@@ -9,7 +13,7 @@ key
 - "checkfull"
 
     // 4, 26, 77, 97 : Móc mạnh lên, được ( 1-80% / 2 -20% ) quả thông rồi, thu thập thêm để đổi quà nhé ! 
-7, 32, 69, 86 : Hộp quà*
+    // 7, 32, 69, 86 : Hộp quà*
     // 16 : Bão tuyết cuốn bạn đến ô 20 rồi, thích thật ! 
     // 20 : Bão tuyết cuốn bạn đến ô 16 rồi, thích thật ! 
     // 18, 56 : Nhặt được đôi vớ, phải làm nhiệm vụ để vượt qua nha, nhiệm vụ lần này : 
@@ -17,13 +21,22 @@ key
     //     Lên ⁠💫┊share chia sẻ 1 tấm hình bạn thấy vui ( 50% ) / 
     //     Treo Voice 15 phút tại Server ( 25% )
     // 27 : Merry Christmas ! 
-28, 96 : Nhắc bạn : Vào #rung-chuông nhận quà nhé ! 
+    // 28, 96 : Nhắc bạn : Vào #rung-chuông nhận quà nhé ! 
     // 41, 72 : Chúc mừng bạn nhặt được 2 quả thông trên đường đi
     // 45 : Chúc mừng bạn luồn qua ống khói đến được ô 61
     // 48, 91 : Chúc mừng bạn nhận thêm 1 xí ngầu ❤️ 
     // 51 : Chúc mừng bạn nhặt được 1 quả thông trên đường đi
     // 58 : Hố đen đưa bạn đến ô 64 rồi, thích thật ! 
     // 64 : Hố đen đưa bạn đến ô 58 rồi, thích thật ! 
+
+Hộp quà : 
+Chúc mừng bạn mở quà được 20.000 owo ( 2/20 )
+Chúc mừng bạn mở quà được 50.000owo ( 1/20 )
+Hộp này hết quà rồi, tìm hộp khác nha ( 4/20 )
+Bạn đã thu thập thêm được 2 quả thông ( 2/20 )
+Bạn đã thu thập thêm được 1 quả thông ( 4/20 )
+Chúc mừng bạn nhận được thêm 1 xí ngầu ( 4/20 )
+Chúc mừng bạn nhận được Role Merry Christmas ! Cùng đón Giáng sinh thôi nào ! ( 3/20 )
 
 16 <-> 20
 45 -> 61
